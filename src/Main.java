6c50e654affa29d5bc84037a1fd460cb9cf778d4
Grajaal <jorgegrajal@gmail.com>
2023-12-07 14:21:08 +0100

    Entrada del usuario completada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.List; 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -14,18 +15,52 @@ public class Main {
             games.add(new ArrayList<String>());
         scanner.nextLine(); 
 
-        for(int i = 0; i < nGames; i++){
-            String row = scanner.nextLine(); 
-            while (!(row = scanner.nextLine().trim()).isEmpty())
-                games.get(i).add(row); 
+        for (int i = 0; i < nGames; i++) {
+            String row = scanner.nextLine();
+            int c = row.length();
+            addRow(row, games.get(i), c);
+
+            do{ 
+                row = scanner.nextLine(); 
+                addRow(row, games.get(i), c);
+            }while(!row.isEmpty()); 
         }
+        
 
         for(int i = 0; i < nGames; i++)
             play(games.get(i));
         
     }
 
-    public static void play(ArrayList<String> game){
-        System.out.println("hola");
+    public static void play(ArrayList<String> gameMatrix){
+        int c = gameMatrix.get(0).length(); 
+        int f = gameMatrix.size(); 
+        char[][] game = new char[f][c]; 
+
+        for(int i = 0; i < f; i ++){
+            for(int j = 0; j < c; j++){
+                game[i][j] = gameMatrix.get(i).charAt(j); 
+            }
+        }
+
+        for(int i = 0; i < f; i ++){
+            for(int j = 0; j < c; j++){
+                System.out.print(game[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void addRow(String input, ArrayList<String> game, int nColumns){
+        if(!input.isEmpty()){
+            char[] rowToChar = input.toCharArray(); 
+            for(char c : rowToChar){
+                if(c != 'A' && c != 'V' && c != 'R')
+                    throw new InputMismatchException("Solo hay fichaf rojas: R, verdes: V y azules: A.");
+            }
+            if(nColumns != rowToChar.length)
+                throw new InputMismatchException("Numero de columnas incorrecto en el juego."); 
+            game.add(input); 
+        }
     }
 }
